929febfd011482d140597a7a50412dfb11504381
brollins <brollins@aa485764-6fe1-c248-a039-611e37cf2447>
2014-01-21 17:33:50 +0000

    Adding empty Lab 05
    git-svn-id: https://svn.neumont.edu:8443/svn/csc150rollinsb@10 aa485764-6fe1-c248-a039-611e37cf2447

---
+++ Lab 05/src/edu/neumont/csc150/lab5/rollinsb/BattleBotCLI.java
@@ -0,0 +1,65 @@
+package edu.neumont.csc150.lab5.rollinsb;
+
+/**
+ * Runs the CLI version of the BattleBot program.
+ * 
+ * @author Blake Rollins
+ * 
+ */
+public class BattleBotCLI {
+
+	/**
+	 * Places a BattleBot on the field
+	 * 
+	 * @param bbField	The BattleBotField to use
+	 * @param botColumn The Bot's starting column
+	 * @param botRow The Bot's starting row
+	 * @param botColumnSpeed The Bot's column movement speed
+	 * @param botRowSpeed The Bot's row movement speed
+	 */
+	public void placeBot(BattleBotField bbField, int botColumn, int botRow, int botColumnSpeed, int botRowSpeed) {
+		bbField.addBot(new BattleBot(botColumn, botRow, botColumnSpeed, botRowSpeed));
+	}
+	
+	/**
+	 * Places four BattleBots on the field in the corners with random speeds
+	 * @param bbField	The BattleBotField to use
+	 */
+	public void placeFourCornerBots(BattleBotField bbField) {
+		bbField.addBot(new BattleBot(0, 0));
+		bbField.addBot(new BattleBot(0, bbField.getNumberOfRows() - 1));
+		bbField.addBot(new BattleBot(bbField.getNumberOfColumns() - 1, 0));
+		bbField.addBot(new BattleBot(bbField.getNumberOfColumns() - 1, bbField.getNumberOfRows() - 1));
+	}
+
+	/**
+	 * The CLI version of the BattleBot program
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		BattleBotCLI program = new BattleBotCLI();		
+		
+		// For the third / fourth lab we are placing 4 Bots in the corners of a board.
+		// We are going to pass in three arguments, the number of cols in 
+		// the board, the number of rows in the board and the number of time units to process before 
+		// printing the board.
+		if (args.length == 3) {
+			int numberOfCols = Integer.parseInt(args[0]);
+			int numberOfRows = Integer.parseInt(args[1]);
+			int numberOfTimeUnits = Integer.parseInt(args[2]);
+			
+			BattleBotField testBoard = new BattleBotField(numberOfCols, numberOfRows);
+			program.placeFourCornerBots(testBoard);
+			testBoard.moveBots(numberOfTimeUnits);
+			System.out.println(testBoard.toString());
+		} else {
+			System.out.println("Invalid number of arguments:");
+			System.out.println("Usage: java edu.neumont.csc150.lab4.rollinsb.BattleBotCLI numberOfCols numberOfRows amountOfTime");
+		}
+		
+
+	}
+
+}
---
+++ Lab 05/src/edu/neumont/csc150/lab5/rollinsb/BattleBotField.java
@@ -0,0 +1,147 @@
+package edu.neumont.csc150.lab5.rollinsb;
+
+/**
+ * The BattleBotField holds an 2 dimensional array of Booleans representing the
+ * locations of the Bots Also holds an array of BattleBot objects
+ * 
+ * @author Blake Rollins
+ * 
+ */
+public class BattleBotField {
+
+	private BattleBot[] bots;
+	private boolean[][] field;
+
+	/**
+	 * Creates a BattleBotField of the specified dimensions. Creates an empty
+	 * array of BattleBots.
+	 * 
+	 * @param numberOfRows
+	 *            The number of rows for the field
+	 * @param numberOfColumns
+	 *            The number of columns for the field
+	 */
+	public BattleBotField(int numberOfColumns, int numberOfRows) {
+		field = new boolean[numberOfColumns][numberOfRows];
+
+		bots = new BattleBot[0];
+	}
+
+	/**
+	 * Adds a BattleBot object to the field
+	 * 
+	 * @param newBot
+	 *            The new BattleBot object to be added
+	 */
+	public void addBot(BattleBot newBot) {
+		BattleBot[] newBots = new BattleBot[bots.length + 1];
+
+		for (int i = 0; i < bots.length; i++) {
+			newBots[i] = bots[i];
+		}
+		newBots[bots.length] = newBot;
+
+		bots = newBots;
+	}
+	
+	/**
+	 * Returns the BattleBot at the specified index
+	 */
+	public BattleBot getBot(int botIndex) {
+		// TODO: Check NullPointer
+		return bots[botIndex];
+	}
+	
+	/**
+	 * Returns the number of BattleBots on the field
+	 * @return	The number of BattleBots on the field
+	 */
+	public int getNumberOfBots() {
+		return bots.length;
+	}
+	
+	/**
+	 * Returns the number of columns on the field
+	 * @return	The number of columns on the field
+	 */
+	public int getNumberOfColumns() {
+		return field.length;
+	}
+	
+	/**
+	 * Returns the number of rows on the field
+	 * @return	The number of rows on the field
+	 */
+	public int getNumberOfRows() {
+		return field[0].length;
+	}
+
+	/**
+	 * Updates the location of each BattleBot on the field by calling the move
+	 * method of each Bot.
+	 */
+	public void moveBots() {
+		for (int i = 0; i < bots.length; i++) {
+			bots[i].move(1);
+		}
+
+	}
+
+	/**
+	 * Updates the location of each BattleBot on the field by calling the move
+	 * method of each Bot.
+	 */
+	public void moveBots(int timeUnits) {
+		for (int i = 0; i < bots.length; i++) {
+			bots[i].move(timeUnits);
+		}
+
+	}
+
+	/**
+	 * Updates the field array with the location of all the Bots.
+	 */
+	public void paintField() {
+		for (int i = 0; i < bots.length; i++) {
+			int botColumn = bots[i].getColumn();
+			int botRow = bots[i].getRow();
+			if (botColumn >= 0 && botColumn < getNumberOfColumns() && botRow >= 0 && botRow < getNumberOfRows()) {
+				field[botColumn][botRow] = true;
+			}
+		}
+	}
+
+	/**
+	 * Sets all locations of the field array to false.
+	 */
+	private void clearField() {
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[0].length; j++) {
+				field[i][j] = false;
+			}
+		}
+	}
+
+	/**
+	 * Returns a String representation of the field array.
+	 */
+	public String toString() {
+		clearField();
+		paintField();
+		
+		String outString = "";
+		// Add the array to the output
+		for (int rowIndex = 0; rowIndex < getNumberOfRows(); rowIndex++) {
+			for (int columnIndex = 0; columnIndex < getNumberOfColumns(); columnIndex++) {
+				outString += (field[columnIndex][rowIndex]) ? "X" : "-";
+			}
+			outString += "\n";
+		}
+		// Print the info for the Bots (in case they are off screen
+		for (int botIndex = 0; botIndex < getNumberOfBots(); botIndex++) {
+			outString += "Bot " + (botIndex + 1) + " is at (" + bots[botIndex].getColumn() + "," + bots[botIndex].getRow() + ")\n";
+		}
+		return outString;
+	}
+
+}
